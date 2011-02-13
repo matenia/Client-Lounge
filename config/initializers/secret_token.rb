416647f84814ff1a4009e21ec626bcf1da2d3ee2
Matenia Rossides <matenia@gmail.com>
2011-02-13 22:26:15 +1100

    started from scratch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clientlounge::Application.config.secret_token = '77321542f1d484cde9aff363dca305e7f24f487bc90224a0e821654307b3b53330af022098391b192b23aa29cd9ed004737d731096a1fa1f1161e479709a313f'
